24aba3109b42ba6539cdb59a89013d56b1f57903
Rahul Babbar <rahulbabbar1@live.com>
2017-03-21 11:58:48 +0530

    Added map fragment

--- app/src/main/java/com/example/user/medicobuddy/home.java
+++ app/src/main/java/com/example/user/medicobuddy/home.java
@@ -1,9 +1,11 @@
 package com.example.user.medicobuddy;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +17,14 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,10 +41,14 @@ public class home extends Fragment implements AdapterView.OnItemSelectedListener
     public home() {
         // Required empty public constructor
     }
+
     TextView med_text;
     private static final String TAG = MainActivity.class.getSimpleName();
     private DatabaseReference mFirebaseDatabase;
     private FirebaseDatabase mFirebaseInstance;
+
+    MapView mMapView;
+    private GoogleMap googleMap;
 
 
     @Override
@@ -48,17 +62,31 @@ public class home extends Fragment implements AdapterView.OnItemSelectedListener
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        med_text=(TextView) view.findViewById(R.id.medicine);
+        med_text = (TextView) view.findViewById(R.id.medicine);
 
         mFirebaseInstance = FirebaseDatabase.getInstance();
 
         final Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
 
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 mFirebaseDatabase = mFirebaseInstance.getReference("Medicine");
-                final String med=spinner.getSelectedItem().toString();
+                final String med = spinner.getSelectedItem().toString();
+
+
 
                 mFirebaseInstance.getReference(med).addValueEventListener(new ValueEventListener() {
                     @Override
@@ -70,6 +98,38 @@ public class home extends Fragment implements AdapterView.OnItemSelectedListener
                             public void onDataChange(DataSnapshot dataSnapshot) {
                                 if(dataSnapshot.exists()){
                                     for(DataSnapshot med : dataSnapshot.getChildren()){
+                                        mMapView.getMapAsync(new OnMapReadyCallback() {
+                                            @Override
+                                            public void onMapReady(GoogleMap mMap) {
+                                                googleMap = mMap;
+
+                                                // For showing a move to my location button
+                                                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                                                        != PackageManager.PERMISSION_GRANTED
+                                                        &&
+                                                        ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                                                                != PackageManager.PERMISSION_GRANTED) {
+                                                    // TODO: Consider calling
+                                                    //    ActivityCompat#requestPermissions
+                                                    // here to request the missing permissions, and then overriding
+                                                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                                    //                                          int[] grantResults)
+                                                    // to handle the case where the user grants the permission. See the documentation
+                                                    // for ActivityCompat#requestPermissions for more details.
+                                                    return;
+                                                }
+                                                googleMap.setMyLocationEnabled(true);
+
+                                                // For dropping a marker at a point on the Map
+
+                                                LatLng sydney = new LatLng(-34, 151);
+                                                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                                                // For zooming automatically to the location of the marker
+                                                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                                                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                                            }
+                                        });
                                         Toast.makeText(getActivity(),med.getValue().toString(),Toast.LENGTH_LONG).show();
                                     }
                                 }
@@ -114,5 +174,29 @@ public class home extends Fragment implements AdapterView.OnItemSelectedListener
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
